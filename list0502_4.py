8f1a534f1f28feaf530e5c24562fa985a1a3f51c
aakazu <39019637+aakazu@users.noreply.github.com>
2020-03-18 22:04:30 +0900

    Update list0502_4.py

--- list0502_4.py
+++ list0502_4.py
@@ -3,7 +3,7 @@ QUESTION=["サザエさんの旦那さんの名前は？","カツオの妹の名
 R_ANS=["マスオ","ワカメ","甥"]
 R_ANS2=["ますお","わかめ","おい"]
 
-for i range(3);
+for i in range(3);
     print(QUESTION[i])
     ans=input()
     if ans == R_ANS[i] or ans == R_ANS2[i]:
